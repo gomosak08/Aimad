f8e20687fd2b5f918deab5146b98582df633cea8
saul <user.name>
2023-11-27 15:24:25 -0600

    Added portfolio items via Resource generatr

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :portfolios
+  get 'page/about'
+  get 'page/contact'
   resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
